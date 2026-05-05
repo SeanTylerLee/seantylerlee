b4b62254ab5b651cd0b00041449da61821123392
SeanTylerLee <seantylerlee@outlook.com>
2026-05-05 10:50:00 -0500

    c

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -6,5 +6,5 @@
  * Last updated for live parser deployment.
  */
 window.SUPABASE_URL = "https://vbonpfrcidaxueumzcve.supabase.co";
-window.SUPABASE_ANON_KEY = "sb_publishable_gKQEknZas24kcEonrx647Q_wJajFVwh";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZib25wZnJjaWRheHVldW16Y3ZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5ODIyMzAsImV4cCI6MjA5MzU1ODIzMH0.Xtk0B0zyNI758raaWX8XeH2EAHqJC7oDrV6elG47E2Q";
 window.SUPABASE_PARSE_FUNCTION = "parse-permit";
